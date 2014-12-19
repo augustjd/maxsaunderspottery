75567e15ea9bf2454a827d59dcf2d72768273a24
Joshua Augustin <augustinspring@gmail.com>
2014-12-19 16:40:40 -0500

    Added api documentation to /api/.

--- sand/views.py
+++ sand/views.py
@@ -14,3 +14,10 @@ def index():
 @app.route('/admin')
 def admin_index():
     return send_from_directory(join(app.static_folder, 'templates/admin'), 'index.html')
+
+
+@app.route('/api/')
+@app.route('/api/documentation')
+@app.route('/api/documentation.html')
+def api_documentation():
+    return send_from_directory(join(app.static_folder, 'templates/api'), 'documentation.html')
